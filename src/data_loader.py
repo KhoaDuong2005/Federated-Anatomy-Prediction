975a3215336c856542006b1c77f5d124be8c4612
Elton <khoa.dhaminh723@gmail.com>
2025-02-05 15:18:46 -0800

    Add a script to transfer file to numpy array

---
+++ src/data_loader.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+
+
+def load_medical_image(file_path):
+    try:
+        image = sitk.ReadImage(file_path)
+        
+        image_array = sitk.GetArrayFromImage(image)
+        
+        if image_array.ndim == 3:
+            middle = image_array.shape[0] // 2
+            image_array = image_array[middle, :, :]
+        
+        image_array = np.array(image_array)
+
+        return image_array
+    except:
+        raise ValueError("File not supported")
+
+def show_image(image_array):
+    plt.imshow(image_array, cmap = "gray")
+    plt.show()
