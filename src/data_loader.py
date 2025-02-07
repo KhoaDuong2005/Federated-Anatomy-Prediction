e8f2507510ee0c578647f83d53e9d5af4fe78c7d
Elton <khoa.dhaminh723@gmail.com>
2025-02-06 17:23:51 -0800

    Change array type so that it work with efficientnet model (3, x, x)

--- src/data_loader.py
+++ src/data_loader.py
@@ -17,11 +17,16 @@ def load_medical_image(file_path, size):
         image_array = np.array(image_array)
         image_array = cv2.resize(image_array, size, interpolation=cv2.INTER_AREA)
 
+        if image_array.ndim == 3:
+            image_array = image_array[:, :, 0]
+
+        image_array = np.stack([image_array] * 3, axis=0)
+
         return image_array
     except:
         raise ValueError("File not supported")
 
 def show_image(image_array):
-    plt.imshow(image_array, cmap = "gray")
+    plt.imshow(image_array[0,:,:], cmap = "gray")
     plt.show()
 
