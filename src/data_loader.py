13fb13c2d6e0905fe1193a9c8eef27fc773735f0
Elton <khoa.dhaminh723@gmail.com>
2025-02-10 14:53:42 -0800

    Re implement the load_images_based_on_config (not-finished)

--- src/data_loader.py
+++ src/data_loader.py
@@ -25,24 +25,59 @@ def load_medical_image(file_path, size):
 def load_images_based_on_config(directory, config, size):
     images = []
     labels = []
-    file_names = []
-
-    if config["label_format"] == "label_folder":
-        for folder_name in os.listdir(directory):
-            folder_path = os.path.join(directory, folder_name)
-            if os.path.isdir(folder_path):
-                is_anatomy = True if folder_name.lower() == "anatomy" else False
-                for file_name in os.listdir(folder_path):
+    file_names_list = []
+    try:
+        if config["label_format"] == "label_folder":
+            for dir_path, dir_names, file_names in os.walk(directory):
+                for file_name in file_names:
+                    file_path = os.path.join(dir_path, file_name)
+                    is_anatomy = dir_path.endswith("anatomy")
+                    try:
+                        print(f"Loading file: {file_path}")
+                        image_array = load_medical_image(file_path, size)
+                        images.append(image_array)
+                        file_names_list.append(file_name)
+                        labels.append(is_anatomy)
+                    except Exception as exception:
+                        print(f"Skipping file: {file_name}: {exception}")
+            return images, file_names_list, labels
+        
+        elif config["label_format"] == "label_file":
+            folder_path = os.path.join(directory, config["image_folder"])
+            for dir_path, dir_names, file_names in os.walk(folder_path):
+                for file_name in file_names:
+                    file_path = os.path.join(dir_path, file_name)
+                    is_anatomy = os.path.splitext(file_name)[0].endswith("_1")
+                    try:
+                        print(f"Loading file: {file_path}")
+                        image_array = load_medical_image(file_path, size)
+                        images.append(image_array)
+                        file_names_list.append(file_name)
+                        labels.append(is_anatomy)
+                    except Exception as exception:
+                        print(f"Skipping file: {file_name}: {exception}")
+            return images, file_names_list, labels
+        
+        elif config["label_format"] == "label_csv":
+            folder_path = os.path.join(directory, config["image_folder"])
+            csv_path = os.path.join(directory, config["label_file_path"])
+            df = pd.read_csv(csv_path, header=None, names=["file_name", "label1", "label2", "label"])
+            print(df)
+            for dir_path, dir_names, file_names in os.walk(folder_path):
+                for file_name in file_names:
                     file_path = os.path.join(folder_path, file_name)
-                    if os.path.isfile(file_path):
-                        try:
-                            image_array = load_medical_image(file_path, size)
-                            images.append(image_array)
-                            file_names.append(file_name)
-                            labels.append(is_anatomy)
-                        except ValueError as e:
-                            print(f"Skipping file: {file_name}: {e}")
-    return images, file_names, labels
+                    try:
+                        is_anatomy = df.loc[df["file_name"] == file_name, "label"].values[0]
+                        image_array = load_medical_image(file_path, size)
+                        images.append(image_array)
+                        file_names_list.append(file_name)
+                        labels.append(is_anatomy)
+                    except Exception as exception:
+                        print(f"Skipping file: {file_name}: {exception}")
+            return images, file_names_list, labels
+    except Exception as exception:
+        print(f"Error when trying to load images based on config: {exception}")
+    
 
 
 
