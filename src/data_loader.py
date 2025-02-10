e6b84ac1eeed7862086495f169b7d8ae85934a37
Elton <khoa.dhaminh723@gmail.com>
2025-02-10 03:36:30 -0800

    Re implement how files saved into mongodb (based on a config.json file)

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,18 +2,14 @@ import numpy as np
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 import cv2
+import os
+import pandas as pd
 
 
 def load_medical_image(file_path, size):
     try:
         image = sitk.ReadImage(file_path)
-
         image_array = sitk.GetArrayFromImage(image)
-        
-        # if (image_array.ndim == 3 and image_array[2] != 3):
-        #     middle = image_array.shape[0] // 2
-        #     image_array = image_array[middle, :, :]
-        
         image_array = np.array(image_array)
         image_array = cv2.resize(image_array, size, interpolation=cv2.INTER_AREA)
 
@@ -25,6 +21,30 @@ def load_medical_image(file_path, size):
         return image_array
     except:
         raise ValueError("File not supported")
+
+def load_images_based_on_config(directory, config, size):
+    images = []
+    labels = []
+    file_names = []
+
+    if config["label_format"] == "label_folder":
+        for folder_name in os.listdir(directory):
+            folder_path = os.path.join(directory, folder_name)
+            if os.path.isdir(folder_path):
+                is_anatomy = True if folder_name.lower() == "anatomy" else False
+                for file_name in os.listdir(folder_path):
+                    file_path = os.path.join(folder_path, file_name)
+                    if os.path.isfile(file_path):
+                        try:
+                            image_array = load_medical_image(file_path, size)
+                            images.append(image_array)
+                            file_names.append(file_name)
+                            labels.append(is_anatomy)
+                        except ValueError as e:
+                            print(f"Skipping file: {file_name}: {e}")
+    return images, file_names, labels
+
+
 
 def show_image(image_array, modality, body_part):
     plt.imshow(image_array[:,:,0], cmap = "gray")
