1dc4b0c00e962dc3ef718361cf1b6740a981d329
Elton <khoa.dhaminh723@gmail.com>
2025-02-11 19:22:15 -0800

    Add saving method for csv_label type

--- src/data_loader.py
+++ src/data_loader.py
@@ -6,21 +6,25 @@ import os
 import pandas as pd
 
 
+
 def load_medical_image(file_path, size):
     try:
         image = sitk.ReadImage(file_path)
         image_array = sitk.GetArrayFromImage(image)
         image_array = np.array(image_array)
-        image_array = cv2.resize(image_array, size, interpolation=cv2.INTER_AREA)
 
-        if image_array.ndim == 3:
+
+        if image_array.ndim == 3 and image_array.shape[2] <= 4:
             image_array = image_array[:, :, 0]
-    
-        image_array = np.stack([image_array] * 3, axis=2)
+        elif image_array.ndim == 3 and image_array.shape[0] <= 3:
+            image_array = image_array[0, :, :]
 
+        image_array = cv2.resize(image_array, size, interpolation=cv2.INTER_AREA)
+        
+        image_array = np.stack([image_array] * 3, axis=2)
         return image_array
-    except:
-        raise ValueError("File not supported")
+    except Exception as exception:
+        raise ValueError(f"File not supported, error: {exception}")
 
 def load_images_based_on_config(directory, config, size):
     images = []
@@ -60,14 +64,17 @@ def load_images_based_on_config(directory, config, size):
         
         elif config["label_format"] == "label_csv":
             folder_path = os.path.join(directory, config["image_folder"])
+
             csv_path = os.path.join(directory, config["label_file_path"])
-            df = pd.read_csv(csv_path, header=None, names=["file_name", "label1", "label2", "label"])
-            print(df)
+            df = pd.read_csv(csv_path)
+            label_dictionary = dict(zip(df["SeriesInstanceUID"], zip(df["normal"], df["abnormal"])))
+
             for dir_path, dir_names, file_names in os.walk(folder_path):
                 for file_name in file_names:
-                    file_path = os.path.join(folder_path, file_name)
+                    file_path = os.path.join(dir_path, file_name)
+                    folder_name = os.path.basename(os.path.dirname(file_path))
                     try:
-                        is_anatomy = df.loc[df["file_name"] == file_name, "label"].values[0]
+                        is_anatomy = label_dictionary[folder_name][1] == 1
                         image_array = load_medical_image(file_path, size)
                         images.append(image_array)
                         file_names_list.append(file_name)
@@ -82,7 +89,8 @@ def load_images_based_on_config(directory, config, size):
 
 
 def show_image(image_array, modality, body_part):
-    plt.imshow(image_array[:,:,0], cmap = "gray")
+    image_array = image_array[:, :, 0]
+    plt.imshow(image_array, cmap = "gray")
     plt.title(f"{modality} {body_part}")
     plt.show()
 
